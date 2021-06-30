a2e89591cd6763669548dc6da787204fa3db7871
Rob Toombs <rtoombs@shieldsrx.com>
2021-06-29 22:04:26 -0400

    Working on getting the DB to persist some entries on button click

--- backend/src/main/kotlin/com/toombs/appointment/AppointmentController.kt
+++ backend/src/main/kotlin/com/toombs/appointment/AppointmentController.kt
@@ -3,6 +3,7 @@ package com.toombs.appointment
 import javax.enterprise.inject.Default
 import javax.inject.Inject
 import javax.ws.rs.GET
+import javax.ws.rs.PUT
 import javax.ws.rs.Path
 
 @Path("/api")
@@ -16,5 +17,11 @@ class AppointmentController {
     @Path("/appointments")
     fun appointments() : List<Appointment> {
         return appointmentService.getAppointments()
+    }
+
+    @PUT
+    @Path("/populate-db")
+    fun populateDatabase() : Boolean {
+        return appointmentService.populateDatabase()
     }
 }
--- backend/src/main/kotlin/com/toombs/appointment/AppointmentService.kt
+++ backend/src/main/kotlin/com/toombs/appointment/AppointmentService.kt
@@ -1,9 +1,11 @@
 package com.toombs.appointment
 
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.inject.Default
 import javax.inject.Inject
-import javax.transaction.Transactional
 
 @ApplicationScoped
 class AppointmentService {
@@ -16,10 +18,32 @@ class AppointmentService {
         return appointmentDAO.listAll()
     }
 
-    @Transactional
-    fun saveAppointments(appointment : Appointment) {
-        appointment.persist()
-      //  appointmentDAO.persist(appointments)
+    fun populateDatabase() : Boolean {
+        for(index in 1..20) {
+            val appt = createAppointment(index)
+            appointmentDAO.persistAndFlush(appt)
+        }
+        return true
     }
 
+    private fun createAppointment(index : Int) : Appointment {
+        val appt = Appointment()
+        appt.lastName = "Last $index"
+        appt.firstName = "First $index"
+        appt.dob = LocalDate.now().minusYears(index.toLong())
+        appt.mrn = "Mrn $index"
+        appt.date = LocalDateTime.now().plusDays(index.toLong())
+        appt.clinician = "Dr. Pants"
+
+        val med = Medication()
+        med.appointment = appt
+        med.name = "Med $index"
+        med.firstFill = LocalDate.now().minusDays(index.toLong())
+        med.copay = BigDecimal.valueOf(index.toLong() + 1)
+        med.daysSupply = index
+
+        appt.medications = listOf(med)
+
+        return appt
+    }
 }
